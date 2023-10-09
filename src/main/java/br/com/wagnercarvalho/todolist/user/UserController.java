c2e2f8e9e45cf51b9103ed4b4919c32aa0a6c4f8
mindlab <wagncarv@outlook.com>
2023-10-09 19:26:53 -0300

    Controller e contexto users

--- src/main/java/br/com/wagnercarvalho/todolist/user/UserController.java
+++ src/main/java/br/com/wagnercarvalho/todolist/user/UserController.java
@@ -1,5 +1,8 @@
 package br.com.wagnercarvalho.todolist.user;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,8 +10,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/users")
 public class UserController {
 
-    public void create() {
-
+    @PostMapping
+    public void create(@RequestBody UserModel user) {
+        System.out.println(user.toString());
     }
 
 }
--- src/main/java/br/com/wagnercarvalho/todolist/user/UserModel.java
+++ src/main/java/br/com/wagnercarvalho/todolist/user/UserModel.java
@@ -6,7 +6,12 @@ import lombok.Setter;
 @Getter
 @Setter
 public class UserModel {
-    private String userName;
+    private String username;
     private String name;
     private String password;
+
+    @Override
+    public String toString() {
+        return String.format("UserModel [userName: %s, name: %s]", this.username, this.name);
+    }
 }
