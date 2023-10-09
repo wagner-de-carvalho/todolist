ae576f6d8f4fd4df4049404a669f5d17dc314986
mindlab <wagncarv@outlook.com>
2023-10-09 19:08:44 -0300

    Configurações iniciais

---
+++ src/main/java/br/com/wagnercarvalho/todolist/user/UserController.java
@@ -0,0 +1,14 @@
+package br.com.wagnercarvalho.todolist.user;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    public void create() {
+
+    }
+
+}
---
+++ src/main/java/br/com/wagnercarvalho/todolist/user/UserModel.java
@@ -0,0 +1,12 @@
+package br.com.wagnercarvalho.todolist.user;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UserModel {
+    private String userName;
+    private String name;
+    private String password;
+}
