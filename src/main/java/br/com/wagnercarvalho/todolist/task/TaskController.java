446bfb4425f5353c4161f7931693434acdd8cfcb
mindlab <wagncarv@outlook.com>
2023-10-12 10:53:01 -0300

    REfatoração controller task

--- src/main/java/br/com/wagnercarvalho/todolist/task/TaskController.java
+++ src/main/java/br/com/wagnercarvalho/todolist/task/TaskController.java
@@ -1,10 +1,13 @@
 package br.com.wagnercarvalho.todolist.task;
 
+import java.util.UUID;
+
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.AllArgsConstructor;
 
 @RestController
@@ -14,7 +17,9 @@ public class TaskController {
     private ITaskRepository taskRepository;
 
     @PostMapping
-    public TaskModel create(@RequestBody TaskModel task) {
+    public TaskModel create(@RequestBody TaskModel task, HttpServletRequest request) {
+        var userId = request.getAttribute("userId");
+        task.setUserId(((UUID) userId));
         return this.taskRepository.save(task);
     }
 
